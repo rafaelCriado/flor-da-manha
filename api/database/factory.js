a72d2f4d6e0b121e6941003d649dd7d5d54c3eef
Rafael Criado <rafael.marquescriado@gmail.com>
2019-12-12 21:17:49 -0300

    Commit innitial

---
+++ api/database/factory.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| Factory
+|--------------------------------------------------------------------------
+|
+| Factories are used to define blueprints for database tables or Lucid
+| models. Later you can use these blueprints to seed your database
+| with dummy data.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use("Factory");
+
+Factory.blueprint("App/Models/User", (faker, i, data = {}) => {
+  return {
+    name: faker.name(),
+    phone: faker.phone({ formatted: false }),
+    email: faker.email(),
+    password: faker.string(),
+    ...data
+  };
+});
